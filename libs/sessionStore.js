3981598f4d126bbcc528c32d145494a3e525c0a9
Trofimov Pavel <pavtrof342@gmail.com>
2017-04-26 16:54:22 +0500

    redis connct creator

--- libs/sessionStore.js
+++ libs/sessionStore.js
@@ -1,13 +1,25 @@
 'use strict';
 const config = require('config');
+const redis = require('redis');
 const redisConf = config.get('redis');
+const url = require('url');
+
+function createClient (port, host, options) {
+  let client;
+
+  if(process.env.REDISTOGO_URL) {
+    const redisUrl = url.parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(redisUrl.port, redisUrl.hostname, options);
+    client.auth(redisUrl.auth.split(':')[1]);
+  } else {
+    client = redis.createClient(port, host, options);
+  }
+
+  return client;
+}
 
 module.exports = function (session) {
   const redisStore = require('connect-redis')(session);
 
-  const connectConfig = process.env.NODE_ENV === 'production'
-    ? { client: process.env.REDISCLOUD_URL }
-    : { host: redisConf.host, port: redisConf.port };
-
-  return new redisStore(connectConfig);
+  return new redisStore({client: createClient(redisConf.port, redisConf.host)});
 };
